b204cea454a1dd1060b0bccaa2406f322063d660
solar05 <artem00298@gmail.com>
2020-11-08 00:34:06 +0300

    Add generation for colour table

--- warehouse/generator/src/generator/core.clj
+++ warehouse/generator/src/generator/core.clj
@@ -1,7 +1,21 @@
 (ns generator.core
-  (:gen-class))
+  (:require
+   [clojure.test :refer :all]
+   [clojure.test.check.generators :as gen]
+   [clojure.string :as s]
+   [clojure.string :as str]))
 
+(def tables ["clothe_colour"])
+
+(def filename "queries.txt")
+
+(def colour-map ["red" "green" "blue" "grey" "brown" "pink"
+                 "black" "white" "yellow" "purple"])
+
+(defn clothe-color []
+  (str "INSERT INTO clothe_colour (colour) VALUES"
+       (apply str
+               (drop-last 2 (s/join "" (map #(str " ('" % "'), ") colour-map)))) ";"))
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (spit filename (clothe-color)))
