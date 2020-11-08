f3660ab1443b6b63e0f3648dd143166db40d1b22
solar05 <artem00298@gmail.com>
2020-11-09 00:02:50 +0300

    Add generation for size

--- warehouse/generator/src/generator/core.clj
+++ warehouse/generator/src/generator/core.clj
@@ -1,21 +1,28 @@
 (ns generator.core
   (:require
-   [clojure.test :refer :all]
    [clojure.test.check.generators :as gen]
-   [clojure.string :as s]
-   [clojure.string :as str]))
+   [clojure.string :as s]))
 
-(def tables ["clothe_colour"])
+(def tables ["clothe_colour" "clothe_size"])
 
 (def filename "queries.txt")
 
 (def colour-map ["red" "green" "blue" "grey" "brown" "pink"
                  "black" "white" "yellow" "purple"])
 
-(defn clothe-color []
+(def size-map ["XS" "S" "M" "L" "XL" "XXL" "XXXL" "4XL"])
+
+(defn clothe-color-gen []
   (str "INSERT INTO clothe_colour (colour) VALUES"
        (apply str
-               (drop-last 2 (s/join "" (map #(str " ('" % "'), ") colour-map)))) ";"))
-(defn -main
-  [& args]
-  (spit filename (clothe-color)))
+              (drop-last 2 (s/join "" (map #(str " ('" % "'), ") colour-map)))) ";"))
+
+(defn clothe-size-gen []
+  (str "INSERT INTO clothe_size (size) VALUES"
+       (apply str
+              (drop-last 2 (s/join "" (map #(str " ('" % "'), ") size-map)))) ";"))
+
+(defn -main [& args]
+  (let [clothe-color (clothe-color-gen)
+        clothe-size (clothe-size-gen)]
+    (spit filename (s/join "\n" [clothe-color clothe-size]))))
