8e80e87ecc3ea10387d42b238e9cec3751fa1f06
solar05 <artem00298@gmail.com>
2020-11-09 03:15:49 +0300

    Refactor strings in generation

--- warehouse/generator/src/generator/core.clj
+++ warehouse/generator/src/generator/core.clj
@@ -7,40 +7,41 @@
 
 (def filename "queries.txt")
 
-(def colour-map ["red" "green" "blue" "grey" "brown" "pink"
-                 "black" "white" "yellow" "purple"])
+(def colour-map ["'red'" "'green'" "'blue'" "'grey'" "'brown'" "'pink'"
+                 "'black'" "'white'" "'yellow'" "'purple'"])
 
-(def size-map ["XS" "S" "M" "L" "XL" "XXL" "XXXL" "4XL"])
+(def size-map ["'XS'" "'S'" "'M'" "'L'" "'XL'" "'XXL'" "'XXXL'" "'4XL'"])
 
-(def position-map [["admin" 50000] ["operator" 45000]
-                   ["seller" 40000] ["consultant" 35000]
-                   ["manager" 42000]])
+(def position-map [["'admin'" 50000] ["'operator'" 45000]
+                   ["'seller'" 40000] ["'consultant'" 35000]
+                   ["'manager'" 42000]])
 
 (defn gender-gen []
-  (gen/generate (gen/elements ["F" "M"])))
+  (gen/generate (gen/elements ["'F'" "'M'"])))
 
 (defn mark-gen []
-  (gen/generate (gen/elements ["Gucci" "Versace" "Burberry" "Louis Vuitton"
-                               "Prada" "D&G" "Chanel" "Giorgio Armani"])))
+  (gen/generate (gen/elements ["'Gucci'" "'Versace'" "'Burberry'" "'Louis Vuitton'"
+                               "'Prada'" "'D&G'" "'Chanel'" "'Giorgio Armani'"])))
 
 (defn price-gen []
   (gen/generate (gen/choose 500 7000)))
 
 (defn material-gen []
-  (gen/generate (gen/elements ["cotton" "flax" "whool" "ramie" "silk" "leather" "fur"])))
+  (gen/generate (gen/elements ["'cotton'" "'flax'" "'whool'"
+                               "'ramie'" "'silk'" "'leather'" "'fur'"])))
 
 (defn type-gen []
-  (gen/generate (gen/elements ["t-shirt" "skirt" "jeans" "socks"
-                               "gloves" "hat" "coat" "shirt" "shorts"])))
+  (gen/generate (gen/elements ["'t-shirt'" "'skirt'" "'jeans'" "'socks'"
+                              "'gloves'" "'hat'" "'coat'" "'shirt'" "'shorts'"])))
 
 (defn article-gen []
-  (let [zero-part (gender-gen)
+  (let [zero-part (gen/generate (gen/elements ["F" "M"]))
         first-part (gen/generate gen/char-alpha)
         second-part (gen/generate (gen/choose 1111 9999))
         third-part (gen/generate gen/char-alpha)
         four-part (gen/generate (gen/choose 1 100))
         last-part (gen/generate gen/char-alpha)]
-    (str zero-part first-part second-part third-part four-part last-part)))
+    (str "'" zero-part first-part second-part third-part four-part last-part "'")))
 
 (defn prepare-handbook-val []
   (let [vals [(type-gen) (article-gen) (mark-gen) (material-gen) (price-gen) (gender-gen)]]
@@ -59,17 +60,17 @@
 (defn clothe-color-gen []
   (str "INSERT INTO clothe_colour (colour) VALUES"
        (apply str
-              (drop-last 2 (s/join "" (map #(str " ('" % "'), ") colour-map)))) ";"))
+              (drop-last 2 (s/join "" (map #(str " (" % "), ") colour-map)))) ";"))
 
 (defn clothe-color-gen []
   (str "INSERT INTO clothe_colour (colour) VALUES"
        (apply str
-              (drop-last 2 (s/join "" (map #(str " ('" % "'), ") colour-map)))) ";"))
+              (drop-last 2 (s/join "" (map #(str " (" % "), ") colour-map)))) ";"))
 
 (defn clothe-size-gen []
   (str "INSERT INTO clothe_size (size) VALUES"
        (apply str
-              (drop-last 2 (s/join "" (map #(str " ('" % "'), ") size-map)))) ";"))
+              (drop-last 2 (s/join "" (map #(str " (" % "), ") size-map)))) ";"))
 
 (defn clothe-example-gen []
   (str "INSERT INTO clothe_example (size_id, colour_id, handbook_id) VALUES "
@@ -78,7 +79,7 @@
 (defn employee-position-gen []
   (str "INSERT INTO employee-position (position-name, position-salary) VALUES "
        (s/join ", " (map
-                     (fn [elem] (str "('" (first elem) "', " (last elem) ")"))
+                     (fn [elem] (str "(" (first elem) ", " (last elem) ")"))
                      position-map)) ";"))
 
 (defn -main [& args]
