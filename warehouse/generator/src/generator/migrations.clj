0ac8ce8f9df28e9bed2d7e27cfd72ec9fc147b23
solar05 <artem00298@gmail.com>
2020-11-18 21:03:03 +0300

    Add employee and clothe in store

--- warehouse/generator/src/generator/migrations.clj
+++ warehouse/generator/src/generator/migrations.clj
@@ -2,7 +2,8 @@
   (:require
    [clojure.test.check.generators :as gen]
    [clojure.string :as s]
-   [faker.generate :as fg]))
+   [faker.generate :as fg]
+   [clojure.zip :as zip]))
 
 (def clothe-colour
   "CREATE TABLE clothe_colour (colour_id serial PRIMARY KEY, colour varchar(20) NOT NULL);")
@@ -19,7 +20,21 @@
 (def employee-position
   "CREATE TABLE employee_position (position_id serial PRIMARY KEY, position_name varchar(20) NOT NULL, position_salary int NOT NULL);")
 
+(def magazine
+  "CREATE TABLE magazine (magazine_id serial PRIMARY KEY, city varchar(50) NOT NULL, street varchar(50) NOT NULL, house int NOT NULL, inn int NOT NULL)")
+
+(def clothe-in-store
+  "CREATE TABLE clothe_in_store (clothe_in_store_id serial PRIMARY KEY, FOREIGN KEY (clothe_id) REFERENCES clothe_example (clothe_id), FOREIGN KEY (magazine_id) REFERENCES magazine (magazine_id), quantity int NOT NULL)")
+
+(def employee
+  "CREATE TABLE employee (employee_id serial PRIMARY KEY, FOREIGN KEY (magazine_id) REFERENCES magazine (magazine_id), FOREIGN KEY (position_id) REFERENCES employee_position (position_id), first_name varchar(50) NOT NULL, last_name varchar(50) NOT NULL, birth_date DATE, hire_date DATE, passport_number int NOT NULL, phone_number varchar(50) NOT NULL);")
+
+;;(map vector [1 2 3] [1 2 3])
+
 (def tables [clothe-colour
              clothe-size
              clothe-handbook
-             employee-position])
+             employee-position
+             magazine
+             clothe-in-store
+             employee])
